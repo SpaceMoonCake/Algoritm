1820aa75740485c521e09106c60812e3383e1285
moonCake <klamina259@gmail.com>
2021-12-22 14:55:49 +0300

    HomeWork3

--- Lesson3/src/main/java/Main.java
+++ Lesson3/src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13};
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18,20};
         System.out.println(findMissingNumber(array));
     }
 
